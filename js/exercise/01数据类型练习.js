1e4cea59312ff69cd811ae94fd47143438b3ca1c
AINILU <1205084629@qq.com>
2021-03-23 05:58:15 +0800

    20

---
+++ js/exercise/01数据类型练习.js
@@ -0,0 +1,13 @@
+var n1 =10,n2=20;
+var n =n1++;//n 10
+console.log('n='+n);//n 10
+console.log('n1='+n1);//n1 11
+n=++n1;//n 12
+console.log('n='+n);//n 12
+console.log('n1='+n1);//n1 12
+n=n2--;//n 20
+console.log('n='+n);//n 20
+console.log('n2='+n2);//n2 19
+n=--n2;
+console.log('n='+n);//n 18
+console.log('n2='+n2);//n2 18
